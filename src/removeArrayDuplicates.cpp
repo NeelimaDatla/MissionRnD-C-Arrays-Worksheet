f7d8232630955d327021cd959777795d110a1ead
Neelimadatla <neelimadatla16@gmail.com>
2015-12-02 22:10:38 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -15,5 +15,45 @@ NOTES: Don't create new array, try to change the input array.
 
 void * removeArrayDuplicates(int *Arr, int len)
 {
-	return NULL;
+	if (len < 0)
+		return NULL;
+	if (Arr == NULL)
+		return NULL;
+	int i;
+	int first = 0;
+	int last = 0;
+	int mid;
+	for (i = 0; i < len;){
+		int first = i + 1;
+		last = len - 1;
+		if (Arr[i] == 0)
+			i++;
+		else{
+			while (first <= last){
+				mid = (first + last) / 2;
+				if (Arr[i] > Arr[mid])
+					first = mid + 1;
+				else if (Arr[i] < Arr[mid])
+					last = mid - 1;
+				else{
+					Arr[mid] = 0;
+				}
+			}
+			i++;
+		}
+	}
+	int j;
+	for (i = 0, j = 0; j < len;){
+		if (Arr[i] != 0){
+			i++;
+			j = i;
+		}
+		else if (Arr[j] == 0)
+			j++;
+		else{
+			Arr[i] = Arr[j];
+			Arr[j] = 0;
+		}
+	}
+	return Arr;
 }
--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -22,11 +22,20 @@ struct student {
 };
 
 struct student ** topKStudents(struct student *students, int len, int K) {
-	if (students == NULL)
-		return NULL;
 	if (len < 0)
 		return NULL;
 	if (K <= 0)
 		return NULL;
-	return NULL;
+	//struct student *who = malloc(sizeof(struct student));
+	//int min_index = 0;
+	//for (int i = 0; len - i != K; i++){
+	//	int min = students[0].score;
+	//	for (int j = 0; j < len; j++){
+	//		if (students[j].score < min && students[j].score != 0)
+	//			min_index = j;
+	//	}
+	//	students[min_index]->score = 0;
+//	}
+//	return &students;
+	
 }
